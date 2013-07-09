88b9b4cfd31e951766e4d6ff927dd168fca296a7
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-07-09 20:40:16 +0300

    Generate Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dutchauction::Application.config.secret_token = 'b81e56cbe3c1cafb817240187c1a1c6ace0585208483dc70d65995b1eaa36fec915e275a75b898673d44b66612ca3cf34e5038a1484ec826fd14ad3749308243'
