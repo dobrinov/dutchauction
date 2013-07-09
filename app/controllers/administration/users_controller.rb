97761e69e29a9e8879cf70f05cfd712dbee7a1fb
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-07-10 01:52:44 +0300

    Make it possible to add users throught the administration

--- app/controllers/administration/users_controller.rb
+++ app/controllers/administration/users_controller.rb
@@ -5,21 +5,34 @@ class Administration::UsersController < Administration::CommonController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      redirect_to administration_users_path, notice: 'User created'
+    else
+      flash.now[:error] = 'User not created'
+      render 'new'
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
   end
 
   def destroy
+    @user = User.find(params[:id])
   end
 
 end
