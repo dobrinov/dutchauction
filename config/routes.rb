28172dcba9c20358b2ef1d9a5ecf4d67c9ad7fea
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-07-10 19:24:48 +0300

    Add the navigation skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Dutchauction::Application.routes.draw do
 
+  resource  :user do
+    resources :purchases, only: []
+  end
+
+  resource  :session, only: [:new, :create, :destroy]
+  resources :auctions, only: [:index, :show]
+
   namespace :administration do
     resources :users
     resources :auctions
     resources :purchases
   end
 
+  root to: "auctions#index"
 end
