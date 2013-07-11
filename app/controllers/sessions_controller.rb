2eaa1b45ca520c9bb5cef4785d5afce0257ef682
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-07-11 18:27:17 +0300

    Implement the auth workflow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,25 @@
 class SessionsController < ApplicationController
 
+  before_filter :require_login, only: ['destroy']
+
   def new
   end
 
   def create
+    user = User.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
+      redirect_to user_path, notice: 'Signed in!'
+    else
+      flash.now[:error] = 'Invalid email or password'
+      render 'new'
+    end
   end
 
   def destroy
+    @_current_user = session[:current_user_id] = nil
+    redirect_to root_path, notice: 'Signed out!'
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,18 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new params[:user]
+
+    if @user.save
+      redirect_to user_path
+    else
+      flash.now[:error] = 'Invalid form data'
+      render :action => 'new'
+    end
   end
 
   def edit
