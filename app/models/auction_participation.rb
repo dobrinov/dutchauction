debfcd3549c20ee68942cb9538807b0ae3ddab89
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-07-12 10:38:49 +0300

    Implement the auction participation workflow

--- app/models/auction_participation.rb
+++ app/models/auction_participation.rb
@@ -5,17 +5,36 @@ class AuctionParticipation < ActiveRecord::Base
   belongs_to :user
 
   # States
-  # state_machine :state, :initial => :new do
+  state_machine :state, :initial => :registered do
 
-  #   state :new
-  #   state :active
-  #   state :in_purchase_flow
-  #   state :timed_out
-  #   state :insufficient_funds
-  #   state :terminated
-  #   state :bought
+    state :registered
+    state :active
+    state :in_purchase_workflow
+    state :canceled
+    state :bought
+    state :timed_out
 
-  # end
+    event :activate do
+      transition [:registered] => :active
+    end
+
+    event :enter_purchase_workflow do
+      transition [:active] => :in_purchase_workflow
+    end
+
+    event :cancel do
+      transition [:in_purchase_workflow] => :canceled
+    end
+
+    event :buy do
+      transition [:in_purchase_workflow] => :bought
+    end
+
+    event :timeout do
+      transition [:in_purchase_workflow] => :timed_out
+    end
+
+  end
   
   # Validations
   # Class methods
