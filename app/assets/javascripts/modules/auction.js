3b6e4b7eb3ea1e5fe36bad50e5ceb7a2c8f2e0c1
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-07-17 19:43:13 +0300

    Remove hardcoded faye addresses

--- app/assets/javascripts/modules/auction.js
+++ app/assets/javascripts/modules/auction.js
@@ -1,7 +1,7 @@
 module('auction', function(auction){
 
   // Subscribe to broadcasts
-  var faye = new Faye.Client('http://localhost:9292/faye');
+  var faye = new Faye.Client($('meta[name=faye]').attr("content"));
 
   faye.subscribe("/auction/" + $(auction).data('id'), function(data) {
     $.ajax({
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,25 @@
 module ApplicationHelper
 
+  def faye_client
+    "#{faye_protocol}#{faye_host}:#{faye_port}/faye"
+  end
+
+  def faye_js
+    "#{faye_protocol}#{faye_host}:#{faye_port}/faye.js"
+  end
+
+  def faye_protocol
+    request.protocol
+  end
+
+  def faye_host
+    request.host
+  end
+
+  def faye_port
+    9292
+  end
+
   def javascript_include_controller_specific_file
     filename = "#{params[:controller]}.js"
 
